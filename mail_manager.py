82a0ed0264c891b800a8183d30a52f9a063fe536
KarolinaZawisza <zawiszakarolina.t@gmail.com>
2021-09-20 22:01:22 +0200

    Adding mail module -#1

---
+++ mail_manager.py
@@ -0,0 +1,13 @@
+import smtplib
+from env import MAIN_EMAIL, MAIN_EMAIL_PASSWORD, MAY_EMAIL
+
+
+def send_email(message):
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=MAIN_EMAIL, password=MAIN_EMAIL_PASSWORD)
+        connection.sendmail(from_addr=MAIN_EMAIL,
+                            to_addrs=MAY_EMAIL,
+                            msg=message.encode('utf-8')
+                            )
+        print(f'Send to {MAY_EMAIL}.')
